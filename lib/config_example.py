5c8d0c3c2a19b9b55efd8d057b3c7d6c493c32fe
lemon913 <glass0213@gmail.com>
2021-05-25 09:39:24 +0900

    Create config_example.py

---
+++ lib/config_example.py
@@ -0,0 +1,5 @@
+# 모든 값을 설정한 후 _example을 파일명에서 지워주세요.
+PPBRequest = "" # 핑퐁빌더 Request URL https://pingpong.us
+PPBToken = "" # 핑퐁빌더 토큰 https://pingpong.us
+DBKRToken = "" # 한국 디스코드 봇 리스트 토큰 https://koreanbots.dev
+BotToken = "" # Discord 애플리케이션 봇 토큰 https://discord.com/developers/applications
